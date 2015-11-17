87d34cc2422bd6a422cd295ec0a97db88cd44504
Vinícius Nascimento Silva <vinicius.nascimento.silva@usp.br>
2015-11-17 18:07:15 -0200

    Inclui a dependencia do módulo do DOM PDF

--- Module.php
+++ Module.php
@@ -4,8 +4,14 @@ namespace ExportaRest;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 
-class Module implements ConfigProviderInterface, AutoloaderProviderInterface
+class Module implements ConfigProviderInterface, AutoloaderProviderInterface, DependencyIndicatorInterface
 {
+
+    public function getModuleDependencies()
+    {
+        return array('DOMPDFModule');
+    }
+
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
